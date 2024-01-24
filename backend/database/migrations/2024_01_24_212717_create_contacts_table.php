b0a09d2cf2330c69b34eeeea7e501b1d52f57eb0
Ignacio Garcia <jigm231998@outlook.com>
2024-01-24 19:27:30 -0300

    feat: Contact model creation and relationship establishment with User

--- backend/database/migrations/2024_01_24_212717_create_contacts_table.php
+++ backend/database/migrations/2024_01_24_212717_create_contacts_table.php
@@ -11,13 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table) {
             $table->uuid('id');
             $table->primary(['id']);
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('address');
+            $table->string('email');
+            $table->string('cellphoneNumber');
+            $table->string('profilePictureURL');
             $table->timestamps();
+            $table->foreignUuid('user_id')
+            ->references('id')
+            ->on('users');
         });
     }
 
@@ -26,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('contacts');
     }
 };
