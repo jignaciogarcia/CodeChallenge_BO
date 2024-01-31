43216eec2359f44d5ee1c5e1fdbc261e65329c74
Ignacio Garcia <jigm231998@outlook.com>
2024-01-31 20:33:28 -0300

    feat: vee-validation integration for login fields validation including error messages

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -8,10 +8,14 @@
                     <div class="pt-6">
                         <label for="email" class="font-sans block">Add</label>
                         <Field type="email" name="email" id="email" placeholder="john@doe.com" rules="required|email"
-                            class="bg-fuchsia-100 h-10 px-4 rounded w-75 md:w-96" />
+                            class="bg-fuchsia-100 h-10 px-4 rounded w-75 md:w-96 block" />
+                        <ErrorMessage name="email" as="div" class="bg-red-200 border-red-500 text-red-950 
+                            p-1 m-1 rounded-md shadow-md text-center" />
                         <label for="password" class="font-sans block pt-2">Password</label>
                         <Field type="password" name="password" id="password" rules="required" 
-                            class="bg-fuchsia-100 h-10 px-4 rounded w-75 md:w-96" />
+                            class="bg-fuchsia-100 h-10 px-4 rounded w-75 md:w-96 block" />
+                        <ErrorMessage name="password" as="div" class="bg-red-200 border-red-500 text-red-950 
+                            p-1 m-1 rounded-md shadow-md text-center" />
                     </div>
                     <div class="flex justify-center">
                         <button class="bg-violet-500 text-white rounded-3xl mt-7 py-2.5 px-20">LOGIN</button>
@@ -23,12 +27,13 @@
 </template>
 
 <script>
-import { Form, Field } from 'vee-validate';
+import { Form, Field, ErrorMessage } from 'vee-validate';
 
 export default {
     components: {
         Form,
-        Field
+        Field,
+        ErrorMessage
     },
     methods: {
         onSubmit(values) {
--- frontend/plugins/vee-validate-plugin.js
+++ frontend/plugins/vee-validate-plugin.js
@@ -1,7 +1,20 @@
 import { defineRule } from 'vee-validate';
-import { required, email } from '@vee-validate/rules';
+import { email } from '@vee-validate/rules';
 
 export default defineNuxtPlugin(nuxtApp => {
-    defineRule('email', email);
-    defineRule('required', required);
-  })
+  defineRule('email', value => {
+    if(email(value) !== true) {
+      return 'The field above must be a valid email.'
+    }
+
+    return true;
+  });
+
+  defineRule('required', value => {
+    if(!value || !value.length) {
+      return 'The field above cannot be empty.'
+    }
+
+    return true;
+  });
+})
