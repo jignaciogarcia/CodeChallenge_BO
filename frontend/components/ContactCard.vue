d6256d1f724b007bd2ccc4aa1f7eb0259b584e39
Ignacio Garcia <jigm231998@outlook.com>
2024-02-03 19:39:39 -0300

    fix: ContactCard styles adjustment

--- frontend/components/ContactCard.vue
+++ frontend/components/ContactCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="rounded-2xl border-2 border-fuchsia-100 w-72">
+    <div class="lg:rounded-2xl lg:border-2 lg:border-fuchsia-100 w-auto lg:bg-white">
         <div class="flex p-4">
             <img :src="contact.profilePictureUrl" alt="Contact profile picture" class="rounded-full h-16 w-16">
             <div class="h-16 w-max truncate px-3 flex items-center">
@@ -8,7 +8,7 @@
                     <p class="font-sans text-sm text-slate-500">{{ contact.email }}</p>
                 </div>
             </div>
-            <img src="../assets/arrowRight.png" class="w-6 h-6 mt-3">
+            <img src="../assets/arrowRight.png" class="w-6 h-6 mt-3 ml-auto">
         </div>
     </div>
 </template>
